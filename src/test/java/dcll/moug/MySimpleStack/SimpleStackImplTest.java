2d06e06091665812c0e44a0003de45243d8fe403
Matesx <matteo.mougeot@gmail.com>
2016-02-23 11:23:07 +0100

    commitTest

--- src/test/java/dcll/moug/MySimpleStack/SimpleStackImplTest.java
+++ src/test/java/dcll/moug/MySimpleStack/SimpleStackImplTest.java
@@ -12,12 +12,17 @@ public class SimpleStackImplTest {
 
     @Before
     public void setUp() throws Exception {
+        simpleStack = new SimpleStackImpl();
+        System.out.println("Je suis exécuté avant chaque test");
 
     }
 
     @Test
     public void testIsEmpty() throws Exception {
-
+        Assert.assertEquals(true, simpleStack.isEmpty());
+        String element1 = "toto";
+        simpleStack.push(new Item(new String(element1)));
+        Assert.assertEquals(false, simpleStack.isEmpty());
     }
 
     @Test
@@ -59,6 +64,17 @@ public class SimpleStackImplTest {
 
     @Test
     public void testPop() throws Exception {
-
+        Item ita = new Item(new Integer(8));
+        Assert.assertEquals(0, simpleStack.getSize());
+        simpleStack.push(ita);
+        Assert.assertEquals(1, simpleStack.getSize());
+        Item o = simpleStack.pop();
+        //on doit retirer l'objet de la pile
+        Assert.assertEquals(0, simpleStack.getSize());
+        //on doit retrouver l'objet initial
+        Assert.assertTrue(o.getValue() instanceof  Integer);
+        Integer integer = (Integer)o.getValue();
+        Assert.assertEquals(8, integer.intValue());
+        Assert.assertEquals(ita.getValue(), integer);
     }
 }
